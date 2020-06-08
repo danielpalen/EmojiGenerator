3befb638ed9ec42fae36c0fa6a48b8d46ad91665
Daniel Palenicek <daniel.palenicek@gmail.com>
2020-06-08 14:23:40 +0200

    worked emoji_reader

--- emoji_reader.py
+++ emoji_reader.py
@@ -4,20 +4,36 @@ import matplotlib.pyplot as plt
 import imageio
 import glob
 
+import numpy as np
+import tensorflow as tf
+
+
 class EmojiReader:
 
     def __init__(self):
-        emoji_data_base_path = './emoji.json' 
-        with open(emoji_data_base_path) as f:
+        self.emojis_base_path = os.path.join('.', 'emoji-data')
+        with open(os.path.join(self.emojis_base_path, 'emoji.json')) as f:
             self.emoji_meta_data = json.load(f)
 
-    def read_images(self, images_base_path, filter=None): # Filter could be a filter functino to select s
-        images = [{
-            'meta' : d,
-            'image': _read_single_image(images_base_path, d['image']) 
-        } for d in self.emoji_meta_data]
+    def read_images(self, filter=None):  # Filter could be a filter functino to select s
+        # images = [{
+        #     'meta': d,
+        #     'image': self._read_single_image(os.path.join(self.emojis_base_path, 'img-apple-64'), d['image'])
+        # } for d in self.emoji_meta_data]
 
         # TODO: return as tf.data.DataSet
+
+        images = [
+            self._read_single_image(os.path.join(
+                self.emojis_base_path, 'img-apple-64'), d['image'])
+            for d in self.emoji_meta_data
+        ]
+        images = [i for i in images if i is not None]
+        images = np.stack(images, axis=0).astype('float32')
+
+        # TODO: pre processing here.
+
+        images = tf.data.Dataset.from_tensor_slices(images)
         return images
 
     def read_images_from_sheet(self):
@@ -32,5 +48,12 @@ class EmojiReader:
             #print(image_name, image_data.shape)
             return image_data
         except:
-            print(f'Error: Could not read image {os.path.join(image_base_path, image_name)}')
+            print(
+                f'Error: Could not read image {os.path.join(image_base_path, image_name)}')
             return None
+
+
+if __name__ == '__main__':
+    reader = EmojiReader()
+    images = reader.read_images()
+    print()
