fdffe06c3b97914108ecc05abff0403a4f50f6ab
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2020-07-07 22:27:50 +0200

    GUI update

--- utilities/helper.py
+++ utilities/helper.py
@@ -31,40 +31,43 @@ IMG_HEIGHT = 256
 
 
 def load(image_file):
-    image=image_file
-    #image = tf.io.read_file(image_file)
-    #image = tf.image.decode_jpeg(image_file)
+    image = image_file
+    # image = tf.io.read_file(image_file)
+    # image = tf.image.decode_jpeg(image_file)
 
     input_image = tf.cast(image, tf.float32)
     return input_image, input_image
 
 
 def resize(input_image, real_image, height, width):
-  input_image = tf.image.resize(input_image, [height, width],
-                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-  real_image = tf.image.resize(real_image, [height, width],
-                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    input_image = tf.image.resize(input_image, [height, width],
+                                  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    real_image = tf.image.resize(real_image, [height, width],
+                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-  return input_image, real_image
+    return input_image, real_image
+
 
 def normalize(input_image, real_image):
-  input_image = (input_image / 127.5) - 1
-  real_image = (real_image / 127.5) - 1
+    input_image = (input_image / 127.5) - 1
+    real_image = (real_image / 127.5) - 1
 
-  return input_image, real_image
+    return input_image, real_image
+
 
 def load_image(image_file):
-  input_image, real_image = load(image_file)
-  input_image, real_image = resize(input_image, real_image,
-                                   IMG_HEIGHT, IMG_WIDTH)
-  input_image, real_image = normalize(input_image, real_image)
+    input_image, real_image = load(image_file)
+    input_image, real_image = resize(input_image, real_image,
+                                     IMG_HEIGHT, IMG_WIDTH)
+    input_image, real_image = normalize(input_image, real_image)
 
-  return input_image, real_image
+    return input_image, real_image
+
 
 def predict_image_pix2pix(image, model_path):
     generator = tf.keras.models.load_model(model_path)
 
-    images=[]
+    images = []
     images.append(imageio.imread(image))
     train_dataset = tf.data.Dataset.from_tensor_slices(images)
 
@@ -72,6 +75,5 @@ def predict_image_pix2pix(image, model_path):
     train_dataset = train_dataset.batch(1)
 
     for inp, tar in train_dataset.take(1):
-        prediction =generator(inp,training=True)
+        prediction = generator(inp, training=True)
         return prediction[0]
-
